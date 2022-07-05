fc81021aaffa1f4d9ad9a65dcb8fb6202953f345
pravash <pravash@pravash-mac.local>
2022-07-04 22:32:28 -0500

    Init springboot project

---
+++ server/src/main/java/edu/miu/amp/AmpApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.amp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpApplication.class, args);
+	}
+
+}
