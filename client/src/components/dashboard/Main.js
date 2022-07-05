86a0ea7d949d6a127bfb48d1f04398028c697e70
Anjana Sharma <developer@danphehealth.com>
2022-07-05 15:10:57 -0500

    added routes and navbar

---
+++ client/src/components/dashboard/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyRoutes from '../MyRoutes'
+import Header from './Header'
+
+export default function Main(){
+    return(
+        <div>
+            <Header></Header>
+            <MyRoutes />
+        </div>
+    )
+}
