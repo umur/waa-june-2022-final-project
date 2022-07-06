c58e7b55c1e83122899d18f4f1766e5625518f73
Mohammad Sharif Stanekzai <103911752+sharifstanekzai@users.noreply.github.com>
2022-07-06 17:49:22 -0500

    silly comment

--- src/main/java/com/cs545waa/waaJune2022finalProject/AlumniManagementPortalApplication.java
+++ src/main/java/com/cs545waa/waaJune2022finalProject/AlumniManagementPortalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlumniManagementPortalApplication {
 
     public static void main(String[] args) {
+        //shgds
         SpringApplication.run(AlumniManagementPortalApplication.class, args);
     }
 
