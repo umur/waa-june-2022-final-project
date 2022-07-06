a318d8b4fe5e320a03691aabff5b552773427753
Pravash Raj Upreti <pravashupreti@gmail.com>
2022-07-06 16:28:14 -0500

    Update the column name for each entity attribute

--- server/src/main/java/edu/miu/amp/domain/JobHistory.java
+++ server/src/main/java/edu/miu/amp/domain/JobHistory.java
@@ -10,16 +10,25 @@ import java.util.List;
 
 @Data
 @Entity
-@AllArgsConstructor@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "company_name")
     private String companyName;
+
+    @Column(name = "start_date")
     private LocalDate startDate;
+
+    @Column(name = "end_date")
     private LocalDate endDate;
+
+    @Column(name = "reason_to_leave")
     private String reasonToLeave;
+
     private String comment;
 
     @ManyToMany
