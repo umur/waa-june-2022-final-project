a2a929b39e0acdb8565bb06cc5044cf370e30011
Anjana Sharma <developer@danphehealth.com>
2022-07-06 18:06:57 -0500

    Profile Page created

--- client/src/components/MyRoutes.js
+++ client/src/components/MyRoutes.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import StudentProfile from './profiles/StudentProfile'
-import Main from './dashboard/Main'
+import UserProfile from './profiles/UserProfile'
 export default function MyRoutes(){
     return(
         <div>
             <Routes>
                 {/* <Route path="/main" element={<Main />}>Main</Route> */}
-                <Route path="/student-profile" element={<StudentProfile />}>My Profile</Route>
+                <Route path="/user-profile" element={<UserProfile />}>My Profile</Route>
             </Routes>
         </div>
     )
