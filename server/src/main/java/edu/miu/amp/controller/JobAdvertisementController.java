ee3e57f04ae2dfbff08c4fb34677e2d42540f755
Ashish Ghimire <ashishghimireg@gmail.com>
2022-07-07 11:37:00 -0500

    error fixed.

--- server/src/main/java/edu/miu/amp/controller/JobAdvertisementController.java
+++ server/src/main/java/edu/miu/amp/controller/JobAdvertisementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/api//job-advertisements")
+@RequestMapping("v1/api/job-advertisements")
 public class JobAdvertisementController {
     @Autowired
     private JobAdvertisementService jobAdvertisementService;
