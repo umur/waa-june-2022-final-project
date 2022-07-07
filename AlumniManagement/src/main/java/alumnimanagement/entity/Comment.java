a0e0973ceb9e6783859073cec6b0018fe93e3568
pbudhathoki <pbudhathoki@miu.edu>
2022-07-06 20:30:12 -0500

    Comment Section CRUD Operation

--- AlumniManagement/src/main/java/alumnimanagement/entity/Comment.java
+++ AlumniManagement/src/main/java/alumnimanagement/entity/Comment.java
@@ -1,8 +1,13 @@
 package alumnimanagement.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Comment {
+    @Id
+    private long id;
+    private String comment;
+
 
 }
