eaf5706f871e0b9e482694e664942ba162ff4342
AKay-Cee <95042223+AKay-Cee@users.noreply.github.com>
2022-07-07 17:29:50 -0500

    Addcomment added

---
+++ alumni_app/src/pages/Cv.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Cv() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- alumni_app/src/pages/comment/AddComment.js
+++ alumni_app/src/pages/comment/AddComment.js
@@ -1,8 +1,50 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
+import { useState } from "react";
 
 export default function AddComment() {
+
+    const [commentState, setCommentState] = useState('');
+    const onClicked = () => {
+
+    }
+
+    const setCommentValue = (e) => {
+        const value = e.target.value;
+        setCommentState(value);
+    }
     return (
-        <> Comment is here
-        </>
+        <div>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 5, height: '3ch', width: '50ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField id="outlined-basic" label="Comment" variant="outlined" onChange={() => {
+                    setCommentValue();
+                }} />
+
+            </Box>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { ml: 5, height: '3ch', width: '10ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+
+                <Button onClick={onClicked} variant="contained">save</Button>
+            </Box>
+
+
+        </div>
+
     )
 }
