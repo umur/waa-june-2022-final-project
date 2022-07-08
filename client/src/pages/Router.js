7b6ebae76d23d3152f2ff1d435c5705764c46532
Pravash Raj Upreti <pravashupreti@gmail.com>
2022-07-08 14:17:55 -0500

    Add student search filter

--- client/src/pages/Router.js
+++ client/src/pages/Router.js
@@ -4,7 +4,7 @@ import {Routes, Route} from 'react-router-dom'
 import UserProfile from  '../components/profiles/UserProfile'
 import Advertisement from '../components/job/Advertisement'
 import Dashboard from '../components/dashboard/Dashboard'
-
+import SearchStudent from './faculty/SearchStudent'
 
 
 
@@ -17,8 +17,8 @@ export default function Router(){
                 {/* <Route path="/main" element={<Main />}>Main</Route> */}
                 <Route path="/user-profile" element={<UserProfile />}>My Profile</Route>
                 <Route path="/add-new-advertisement" element={<Advertisement />}>Add New Advertisement</Route>
-
                 <Route path="/dashboard" element={<Dashboard />}>Dashboard</Route>
+                <Route path="/faculty/search-student" element = {<SearchStudent/> }> Search Student</Route>
 
             </Routes>
         </div>
