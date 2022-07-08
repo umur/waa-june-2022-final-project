588318fa9c61747e1c78901cb70bb101ad79fdc9
pbudhathoki <pbudhathoki@miu.edu>
2022-07-07 21:50:53 -0500

    Add Job and Comment section modification 2

--- alumni_app/src/setup/routes-manager/RouteApp.js
+++ alumni_app/src/setup/routes-manager/RouteApp.js
@@ -6,6 +6,7 @@ import StudentList from "../../pages/student/StudentList";
 import Jobs from "../../pages/job/Jobs";
 import AddComment from "../../pages/comment/AddComment";
 import ErrorPage from "../../pages/ErrorPage";
+import CreateJob from "../../pages/job/CreateJob";
 
 
 export default function RouteApp() {
@@ -16,7 +17,7 @@ export default function RouteApp() {
                 <Route path='/Students' element={<StudentList />}></Route>
                 <Route path='/Faculties' element={<FacultyList />}></Route>
                 <Route path='/AddComment/:id' element={<AddComment />}></Route>
-                <Route path='/Jobs' element={<Jobs />}></Route>
+                <Route path='/Jobs' element={<CreateJob />}></Route>
                 <Route path='*' element={<ErrorPage />}></Route>
             </Routes>
         </>
