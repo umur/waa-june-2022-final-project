d17bd8efe46b2df509bb7a9adb71cff77810f7f7
Ashish Ghimire <ashishghimireg@gmail.com>
2022-07-08 15:12:39 -0500

    Faculty Registration form added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/dashboard/Main';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/dashboard/Main";
 
-import Router from './pages/Router';
-import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom';
-
+import Router from "./pages/Router";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import Registration from "./pages/faculty/Registration";
 
 function App() {
   return (
--- client/src/pages/Router.js
+++ client/src/pages/Router.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import UserProfile from '../components/profiles/UserProfile'
-export default function Router(){
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import UserProfile from "../components/profiles/UserProfile";
+import Dashboard from "../components/dashboard/Dashboard";
+import { Registration as FacultyRegistration } from "../pages/faculty/Registration";
 
-    import UserProfile from './profiles/UserProfile'
-export default function MyRoutes(){
-    return(
-        <div>
-            <Routes>
-                {/* <Route path="/main" element={<Main />}>Main</Route> */}
-                <Route path="/user-profile" element={<UserProfile />}></Route>
+export default function MyRoutes() {
+  return (
+    <div>
+      <Routes>
+        {/* <Route path="/main" element={<Main />}>Main</Route> */}
+        <Route path="/user-profile" element={<UserProfile />}></Route>
 
-                <Route path="/dashboard" element={<Dashboard />}>Dashboard</Route>
-
-            </Routes>
-        </div>
-    )
+        <Route path="/dashboard" element={<Dashboard />}>
+          Dashboard
+        </Route>
+        <Route
+          path="/faculty-registration"
+          element={<FacultyRegistration />}
+        ></Route>
+      </Routes>
+    </div>
+  );
 }
