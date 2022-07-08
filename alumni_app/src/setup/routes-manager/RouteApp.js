e2308b5323bf1e9d9de972b94fb0b369a94ddbbd
Sudip Adhikari <31766056+sudipadhikari01@users.noreply.github.com>
2022-07-07 21:04:35 -0500

    profile components added

--- alumni_app/src/setup/routes-manager/RouteApp.js
+++ alumni_app/src/setup/routes-manager/RouteApp.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import ChangePassword from "../../pages/dashboard/change-password/ChangePassword";
 import HomePage from "../../pages/dashboard/HomePage";
 import FacultyList from "../../pages/faculty/FacultyList";
 import Profile from "../../pages/profile/Profile";
 import StudentList from "../../pages/student/StudentList";
 
-
 export default function RouteApp() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<HomePage />}></Route>
-                <Route path='/Students' element={<StudentList />}></Route>
-                <Route path='/Faculties' element={<FacultyList />}></Route>
-                <Route path='/Jobs' element={<HomePage />}></Route>
-                <Route path='/Profile' element={<Profile/>}></Route>
-                
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/Students" element={<StudentList />}></Route>
+        <Route path="/Faculties" element={<FacultyList />}></Route>
+        <Route path="/Jobs" element={<HomePage />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
+        <Route path="/reset-password" element={<ChangePassword />}></Route>
+      </Routes>
+    </>
+  );
 }
