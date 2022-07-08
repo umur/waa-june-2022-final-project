ec833cc569c0edc543a78120fd101358fd71092f
Pravash Raj Upreti <pravashupreti@gmail.com>
2022-07-08 12:27:45 -0500

    Add  student registration form

--- client/src/pages/Router.js
+++ client/src/pages/Router.js
@@ -4,7 +4,7 @@ import {Routes, Route} from 'react-router-dom'
 import UserProfile from  '../components/profiles/UserProfile'
 import Advertisement from '../components/job/Advertisement'
 import Dashboard from '../components/dashboard/Dashboard'
-
+import Registration from './student/Registration'
 
 
 
@@ -14,12 +14,10 @@ export default function Router(){
     return(
         <div>
             <Routes>
-                {/* <Route path="/main" element={<Main />}>Main</Route> */}
                 <Route path="/user-profile" element={<UserProfile />}>My Profile</Route>
                 <Route path="/add-new-advertisement" element={<Advertisement />}>Add New Advertisement</Route>
-
                 <Route path="/dashboard" element={<Dashboard />}>Dashboard</Route>
-
+                <Route path="/student/registration" element={<Registration/>}> Registration</Route>
             </Routes>
         </div>
     )
