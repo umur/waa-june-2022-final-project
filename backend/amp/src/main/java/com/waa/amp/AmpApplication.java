9ec803c0ba0aa0791ebbc3f1c44eff086638829a
muztaba <hasanat.muztaba@gmail.com>
2022-07-08 19:29:33 -0500

    Backend init

---
+++ backend/amp/src/main/java/com/waa/amp/AmpApplication.java
@@ -0,0 +1,13 @@
+package com.waa.amp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpApplication.class, args);
+	}
+
+}
---
+++ backend/amp/src/test/java/com/waa/amp/AmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waa.amp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
