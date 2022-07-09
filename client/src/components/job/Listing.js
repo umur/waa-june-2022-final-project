3dc4497bb0b51c4579af1a5583662b94283c610c
Anjana Sharma <developer@danphehealth.com>
2022-07-09 11:42:23 -0500

    Added apply button on Job listings

--- client/src/components/job/Listing.js
+++ client/src/components/job/Listing.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Card, Container } from 'react-bootstrap'
+import { Card, Stack, Button } from 'react-bootstrap'
 export default function JobListing(props) {
     return (
         <div className='container'>
-            <Card border="primary" style={{ width: '18rem' }}>
+
+            <Card border="primary">
                 <Card.Header>{props.title}</Card.Header>
                 <Card.Body>
                     <Card.Title>{props.location}</Card.Title>
@@ -14,8 +15,16 @@ export default function JobListing(props) {
                         Job Description:{props.description}
                     </Card.Text>
                 </Card.Body>
+                <Card.Footer>
+                    <Stack direction="horizontal" gap={2}>
+                        <Button as="a" variant="primary" className="align-right">
+                            Apply
+                        </Button>
+                    </Stack>
+                </Card.Footer>
             </Card>
-            <br/>
+
+            <br />
         </div>
     )
 }
