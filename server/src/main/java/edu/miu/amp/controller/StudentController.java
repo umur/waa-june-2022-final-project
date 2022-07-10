f0cf9f98af160d8c559aca94ef9de442ccddece6
schn916 <sachin8peace@gmail.com>
2022-07-10 13:46:59 -0500

    Added cross origin

--- server/src/main/java/edu/miu/amp/controller/StudentController.java
+++ server/src/main/java/edu/miu/amp/controller/StudentController.java
@@ -7,10 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
-
 @RestController
 @RequestMapping("v1/api/students")
 @AllArgsConstructor
