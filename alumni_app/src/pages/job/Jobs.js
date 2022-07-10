3d2d05ce969692d3588f276c9fa2aa7ed607f8eb
Sudip Adhikari <31766056+sudipadhikari01@users.noreply.github.com>
2022-07-09 21:10:52 -0500

    Job Controller updated

--- alumni_app/src/pages/job/Jobs.js
+++ alumni_app/src/pages/job/Jobs.js
@@ -1,111 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import { useNavigate } from "react-router";
 import TableMain from "../../common/TableMain";
 import AutoCompleteSelect from "../../common/AutoCompleteSelect";
 import { Box } from "@mui/material";
 
-
 const listParams = {
-    'showDetail': true,
-    'showEdit': false,
-    'showDelete': true,
-    'showAddComment': false,
-    'dataUrl': "/jobs/''/''/''/''",
-    'addCommentUrl': '//',
-    'editUrl': '',
-    'deleteUrl': '/',
-    detailUrl: "/JobDetails/",
-}
+  showDetail: true,
+  showEdit: false,
+  showDelete: true,
+  showAddComment: false,
+  dataUrl: "/jobs/''/''/''/''",
+  addCommentUrl: "//",
+  editUrl: "",
+  deleteUrl: "/",
+  detailUrl: "/JobDetails/",
+};
 
 export default function Jobs() {
-    const navigate = useNavigate();
-    const [state, setState] = useState();
-    const [city, setCity] = useState();
-    const [tag, setTag] = useState();
-    const [name, setName] = useState();
-    const [listParamsD, setlistParamsD] = useState(listParams);
+  const navigate = useNavigate();
+  const [state, setState] = useState();
+  const [city, setCity] = useState();
+  const [tag, setTag] = useState();
+  const [name, setName] = useState();
+  const [listParamsD, setlistParamsD] = useState(listParams);
 
-    const setValue = (name, value) => {
-        if (value[0] != null) {
-            switch (name) {
-                case 'state':
-                    setState(value[0].title);
-                    break;
-                case 'city':
-                    setCity(value[0].title);
-                    break;
-                case 'tag':
-                    setTag(value[0].title);
-                    break;
-                case 'name':
-                    setName(value[0].title);
-                    break;
-            }
-        }
+  const setValue = (name, value) => {
+    if (value[0] != null) {
+      switch (name) {
+        case "state":
+          setState(value[0].title);
+          break;
+        case "city":
+          setCity(value[0].title);
+          break;
+        case "tag":
+          setTag(value[0].title);
+          break;
+        case "name":
+          setName(value[0].title);
+          break;
+      }
     }
+  };
 
-    const reLoadList = () => {
-        const url = '/jobs/' + state + "/" + city + "/" + tag + "/" + name;
-        setlistParamsD({ ...listParamsD, 'dataUrl': url })
-    }
+  const reLoadList = () => {
+    const url = "/jobs/" + state + "/" + city + "/" + tag + "/" + name;
+    setlistParamsD({ ...listParamsD, dataUrl: url });
+  };
 
-    return (
-        <>
-            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <Button variant="contained" color="primary" onClick={() => (navigate('/Jobs/newJob'))}>
-                    Add Job
-                </Button>
-            </div>
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate("/Jobs/newJob")}
+        >
+          Add Job
+        </Button>
+      </div>
 
-            <span className="list-header">  Job List </span>
+      <span className="list-header"> Job List </span>
 
-            <div className="row body-custom">
-                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)' }}>
-                    <div>
-                        < AutoCompleteSelect
-                            name="state"
-                            onChange={value => setValue('state', value)}
-                            isMultiSelect={false}
-                            label={"State"}
-                            placeholder={"Eg. iowa, california"}
-                            dataUrl='/reports/state' ></AutoCompleteSelect >
-                    </div>
-                    <div>
-                        < AutoCompleteSelect
-                            name="city"
-                            onChange={value => setValue('city', value)}
-                            isMultiSelect={false}
-                            label={"City"}
-                            placeholder={"Eg. fairfield, ottumwa"}
-                            dataUrl='/reports/city' ></AutoCompleteSelect >
-                    </div>
-                    <div>
-                        < AutoCompleteSelect
-                            name="tag"
-                            onChange={value => setValue('tag', value)}
-                            isMultiSelect={false}
-                            label={"Tag"}
-                            placeholder={"Eg. mpp, fpp"}
-                            dataUrl='/tags' ></AutoCompleteSelect >
-                    </div>
-                    <div>
-                        < AutoCompleteSelect
-                            name="name"
-                            onChange={value => setValue('name', value)}
-                            isMultiSelect={false}
-                            label={"Company Name"}
-                            placeholder={"Eg. messi, ronaldo, pele"}
-                            dataUrl='/reports/studentName' ></AutoCompleteSelect >
-                    </div>
-                    <div>
-                        <Button variant="contained" color="primary" onClick={reLoadList}>Search</Button>
-                    </div>
-                </Box>
-            </div>
-            <div className="list-table">
-                <TableMain listParams={listParamsD}></TableMain>
-            </div>
-        </>
-    )
+      <div className="row body-custom">
+        <Box sx={{ display: "grid", gridTemplateColumns: "repeat(5, 1fr)" }}>
+          <div>
+            <AutoCompleteSelect
+              name="state"
+              onChange={(value) => setValue("state", value)}
+              isMultiSelect={false}
+              label={"State"}
+              placeholder={"Eg. iowa, california"}
+              dataUrl="/reports/state"
+            ></AutoCompleteSelect>
+          </div>
+          <div>
+            <AutoCompleteSelect
+              name="city"
+              onChange={(value) => setValue("city", value)}
+              isMultiSelect={false}
+              label={"City"}
+              placeholder={"Eg. fairfield, ottumwa"}
+              dataUrl="/reports/city"
+            ></AutoCompleteSelect>
+          </div>
+          <div>
+            <AutoCompleteSelect
+              name="tag"
+              onChange={(value) => setValue("tag", value)}
+              isMultiSelect={false}
+              label={"Tag"}
+              placeholder={"Eg. mpp, fpp"}
+              dataUrl="/tags"
+            ></AutoCompleteSelect>
+          </div>
+          <div>
+            <AutoCompleteSelect
+              name="name"
+              onChange={(value) => setValue("name", value)}
+              isMultiSelect={false}
+              label={"Company Name"}
+              placeholder={"Eg. messi, ronaldo, pele"}
+              dataUrl="/reports/studentName"
+            ></AutoCompleteSelect>
+          </div>
+          <div>
+            <Button variant="contained" color="primary" onClick={reLoadList}>
+              Search
+            </Button>
+          </div>
+        </Box>
+      </div>
+      <div className="list-table">
+        <TableMain listParams={listParamsD}></TableMain>
+      </div>
+    </>
+  );
 }
