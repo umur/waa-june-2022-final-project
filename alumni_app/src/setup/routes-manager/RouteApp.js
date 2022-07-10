c4d3abb5b9a757e1a7627f295eee666a34005785
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-07-09 22:09:29 -0500

    Route Fixed

--- alumni_app/src/setup/routes-manager/RouteApp.js
+++ alumni_app/src/setup/routes-manager/RouteApp.js
@@ -9,8 +9,6 @@ import StudentList from "../../pages/student/StudentList";
 import Profile from "../../pages/profile/Profile";
 import StudentDetail from "../../pages/student/StudentDetail";
 import CreateJob from "../../pages/job/CreateJob";
-import AutoCompleteSelect from "../../common/AutoCompleteSelect";
-import TestAuto from "../../common/testAuto";
 import Jobs from "../../pages/job/Jobs";
 import FileUpload from "../../common/FileUpload";
 import CommentList from "../../pages/comment/CommentList";
@@ -32,15 +30,9 @@ export default function RouteApp() {
         <Route path="/StudentDetails/:id" element={<StudentDetail />}></Route>
         <Route path="/Profile" element={<Profile />}></Route>
         <Route path="/reset-password" element={<ChangePassword />}></Route>
-
         <Route path="/Jobs/newJob" element={<CreateJob />}></Route>
         <Route path="/FileUpload" element={<FileUpload />}></Route>
         <Route path="/JobDetails/:id" element={<JobDetails />}></Route>
-
-        <Route path="/as" element={<TestAuto />}></Route>
-
-
-
       </Routes>
     </>
   );
