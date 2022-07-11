4610b79b75e0e953ddd7e6e0d2bdb628b71554c5
Ashish Ghimire <ashishghimireg@gmail.com>
2022-07-11 14:21:36 -0500

    Last 10 Job Advertisement Query.

--- server/src/main/java/edu/miu/amp/repository/JobAdvertisementRepository.java
+++ server/src/main/java/edu/miu/amp/repository/JobAdvertisementRepository.java
@@ -2,8 +2,20 @@ package edu.miu.amp.repository;
 
 import edu.miu.amp.domain.JobAdvertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JobAdvertisementRepository extends JpaRepository<JobAdvertisement, Integer> {
+
+
+    //@Query("SELECT TOP 25 a FROM Arcust a WHERE a.arcustno<='?1' ORDER BY a.arcustno DESC")
+
+
+//    @Query ("SELECT a FROM JobAdvertisement  a where a.id <='?1' ORDER BY a.id DESC")
+    public List<JobAdvertisement> findTop10ByOrderById() ;
+
+
 }
