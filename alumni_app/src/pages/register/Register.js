cd7fafaa7644253cceb0689a7b0f24ef910eb58b
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-07-12 10:37:47 -0500

    Register UI

--- alumni_app/src/pages/register/Register.js
+++ alumni_app/src/pages/register/Register.js
@@ -42,19 +42,18 @@ function Register() {
     return (
         <div className="">
             <section className="" >
-                <div className="container h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="row d-flex justify-content-center align-items-center">
                         <div className="col-lg-12 col-xl-11">
                             <div className="card text-black" >
-                                <div className="card-body p-md-5">
+                                <div className="card-body ">
                                     <div className="row justify-content-center">
                                         <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                                            <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                                            <p className="text-center h1 fw-bold mb-5 ">Sign up</p>
 
                                             <form className="mx-1 mx-md-4">
 
-                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                <div className="d-flex flex-row align-items-center ">
                                                     <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input
@@ -68,7 +67,7 @@ function Register() {
                                                     </div>
                                                 </div>
 
-                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                <div className="d-flex flex-row align-items-center">
                                                     <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input
@@ -95,7 +94,7 @@ function Register() {
                                                     </div>
                                                 </div>
 
-                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                <div className="d-flex flex-row align-items-center ">
                                                     <i className="fas fa-key fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input
@@ -173,7 +172,6 @@ function Register() {
                             </div>
                         </div>
                     </div>
-                </div>
             </section>
         </div>
     )
