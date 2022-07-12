a93864c73e02269b773de5dd9cef9e63b8ca4675
Pravash Raj Upreti <pravashupreti@gmail.com>
2022-07-12 16:05:28 -0500

    Integrate api

--- client/src/redux/reducers/JobHistory/reducer.js
+++ client/src/redux/reducers/JobHistory/reducer.js
@@ -1,16 +1,25 @@
-import { ADD_JOB_HISTORY, GET_JOB_HISTORY } from "./actionsTypes";
+import {
+  ADD_JOB_HISTORY,
+  GET_JOB_HISTORY,
+  JOB_HISTORY_LIST,
+} from "./actionsTypes";
 
 import { SUCCESS_SUFFIX } from "redux-axios-middleware";
 
 const jobAdvertisementReducer = (state = { jobHistory: [] }, action) => {
   switch (action.type) {
+    case JOB_HISTORY_LIST + SUCCESS_SUFFIX:
+      return {
+        ...state,
+        jobHistory: [...action.payload.data],
+      };
+
     case ADD_JOB_HISTORY + SUCCESS_SUFFIX:
-      console.log(state);
       return {
         ...state,
         jobHistory: [...state.jobHistory, action.payload.data],
       };
-    
+
     case GET_JOB_HISTORY + SUCCESS_SUFFIX:
       return action.payload.data;
     // case DELETE_JOB_ADVERTISEMENT:
