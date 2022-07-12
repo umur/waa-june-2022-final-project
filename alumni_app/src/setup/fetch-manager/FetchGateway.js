c4a116514e8cb1f3953956b4f5ba407fc4596863
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-07-11 23:41:49 -0500

    File Post Token

--- alumni_app/src/setup/fetch-manager/FetchGateway.js
+++ alumni_app/src/setup/fetch-manager/FetchGateway.js
@@ -10,7 +10,7 @@ export async function getRequest(path) {
     try {
         const config = {
             headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhc2hpc2hAbWl1LmVkdSIsInJvbGVzIjoiQURNSU4iLCJpZCI6MSwidXNlck5hbWUiOiJhc2hpc2hAbWl1LmVkdSIsImV4cCI6MTY1NzYxNjc0MiwiaWF0IjoxNjU3NTk4NzQyfQ.n3Hrsd6u9nBvkza7TBeZGntLG0zQ95cAP23kywy93U3aTWPYfpCOr-nFEhR_K-7yHm6KoRPiKD06im5ZmfVvXg'
+                Authorization: auth
             },
         };
 
