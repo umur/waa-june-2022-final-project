025094b6d2cc040347572c74fb82bbf44470aee1
muztaba <hasanat.muztaba@gmail.com>
2022-07-12 16:34:18 -0500

    admin get all user

--- backend/amp/src/main/java/com/waa/amp/controller/AdminController.java
+++ backend/amp/src/main/java/com/waa/amp/controller/AdminController.java
@@ -6,13 +6,9 @@ import com.waa.amp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.Map;
 
 import static java.util.Map.of;
 
