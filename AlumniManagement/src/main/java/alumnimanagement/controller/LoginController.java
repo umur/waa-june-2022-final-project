f9a7b9d3b9629da6f5e1dd9b21e5382968d2582a
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-07-12 15:38:39 -0500

    Dashboard Design

--- AlumniManagement/src/main/java/alumnimanagement/controller/LoginController.java
+++ AlumniManagement/src/main/java/alumnimanagement/controller/LoginController.java
@@ -17,7 +17,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/login")
@@ -34,7 +33,6 @@ public class LoginController {
     private UserAuthServiceImpl userAuthService;
 
     private String token;
-
 
     private PasswordEncoder passwordEncoder;
     @PostMapping()
