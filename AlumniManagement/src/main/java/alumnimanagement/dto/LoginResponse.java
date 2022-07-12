344e05110cdc6e18c24e7ea5b71ca25d48acf992
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-07-12 13:58:47 -0500

    Login Response

---
+++ AlumniManagement/src/main/java/alumnimanagement/dto/LoginResponse.java
@@ -0,0 +1,10 @@
+package alumnimanagement.dto;
+
+public class LoginResponse {
+    public long id;
+    public String token;
+    public String Role;
+    public boolean isStudent;
+    public boolean isFaculty;
+    public boolean isAdmin;
+}
